cceaf2d925f67a5559547abb18ead89c08f64f82
Arsdev998 <arsdev998@gmail.com>
2024-05-08 09:09:11 +0700

    setup databe and rest api

---
+++ src/components/UserList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserList = () => {
+  return (
+    <div className="flex justify-center mt-5">
+      <div className="">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum, sunt
+        rem id voluptas quaerat modi placeat veritatis officiis et illo expedita
+        laudantium ab, odit ratione quae minima non culpa. Est.
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
